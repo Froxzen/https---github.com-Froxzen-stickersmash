0aadc1b8a4ef33c4891931a93d0455123e5fe8b9
Froxzen <harvardchong1@gmail.com>
2025-06-29 23:24:10 +1000

    Added tabs

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -6,7 +6,7 @@ export default function NotFoundScreen() {
 		<>
 			<Stack.Screen options={{title: 'Oops! Not found'}} />
 			<View style={styles.container}>
-				<Link href="/" style={styles.button}>
+				<Link href="/(tabs)" style={styles.button}>
 				Go back to Home screen!
 				</Link>
 			</View>

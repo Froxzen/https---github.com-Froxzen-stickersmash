e2906043fd582dc4113b4f493ca489914e43d562
Froxzen <harvardchong1@gmail.com>
2025-06-29 23:47:25 +1000

    Added image

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,30 +1,38 @@
-import { Link } from "expo-router";
-import { StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, View } from "react-native";
+
+const PlaceholderImage = require("@/assets/images/Suit.png");
 
 export default function Index() {
-	return (
-		<View style={styles.container}>
-			<Text style={styles.text}>Home Screen</Text>
-      <Link href="/about" style={styles.button}>
-        Go to About
-      </Link>
-		</View>
-	);
+  return (
+    <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        <Image source={PlaceholderImage} style={styles.image} />
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: "#25292e",
-		alignItems: "center",
-		justifyContent: "center",
-	},
-	text: {
-		color: "#fff",
-	},
+  container: {
+    flex: 1,
+    backgroundColor: "#25292e",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    color: "#fff",
+  },
   button: {
     fontSize: 20,
-    textDecorationLine: 'underline',
-    color: "#fff"
-  }
+    textDecorationLine: "underline",
+    color: "#fff",
+  },
+  imageContainer: {
+    flex: 1,
+  },
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
+  },
 });
